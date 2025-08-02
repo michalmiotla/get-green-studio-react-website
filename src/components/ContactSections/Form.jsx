cfa68d45180a2f412df1ca53d5501c84708f299e
Michał Miotła <miotlamichal@gmail.com>
2025-08-02 19:19:53 +0200

    working with contact section

--- src/components/ContactSections/Form.jsx
+++ src/components/ContactSections/Form.jsx
@@ -16,14 +16,87 @@ export function Form() {
 					</p>
 				</div>
 				<div className={styles.main}>
-					<div className={styles.image}>
-						<img src={MENTION} alt='' />
-					</div>
-					<hr className={styles.horizontal_line_two} />
-					<hr className={styles.vertical_line} />
-					<div className={styles.form_box}>
-						<p className={styles.disclaimer}></p>
-					</div>
+					<form autoComplete='off' onSubmit={() => console.log('ok')}>
+						<div className={styles.basic_info}>
+							<div className={styles.image}>
+								<img src={MENTION} alt='' />
+							</div>
+							<hr className={styles.horizontal_line_two} />
+							<hr className={styles.vertical_line} />
+							<div className={styles.basic_box}>
+								{/* <span className={styles.required}>*Pole wymagane</span> */}
+								<div className={styles.basic_info_box}>
+									<label className={styles.bigger_label} htmlFor='name'>
+										Imię:*
+									</label>
+									<input className={styles.basic_input} id='name' type='text' />
+									<span className={styles.error}>Błędne dane</span>
+								</div>
+								<div className={styles.basic_info_box}>
+									<label className={styles.bigger_label} htmlFor='mail'>
+										Adres e-mail:*
+									</label>
+									<input className={styles.basic_input} id='mail' type='mail' />
+									<span className={styles.error}>Błędne dane</span>
+								</div>
+								<div className={styles.basic_info_box}>
+									<label className={styles.bigger_label} htmlFor='city'>
+										Miejscowość:*
+									</label>
+									<input className={styles.basic_input} id='city' type='text' />
+									<span className={styles.error}>Błędne dane</span>
+								</div>
+								<div className={styles.basic_info_box}>
+									<label className={styles.bigger_label} htmlFor='area'>
+										Powierzchnia działki:*
+									</label>
+									<input className={styles.basic_input} id='area' type='text' />
+									<span className={styles.error}>Błędne dane</span>
+								</div>
+							</div>
+						</div>
+						<div className={styles.second_info}>
+							<div className={styles.second_info_box}>
+								<p className={styles.bigger_label}>Preferowany pakiet:</p>
+								<input type='radio' name='mini' />
+								<label className={styles.smaller_label} htmlFor='mini'>
+									Green Mini
+								</label>
+								<input type='radio' name='standard' />
+								<label className={styles.smaller_label} htmlFor='standard'>
+									Green Standard
+								</label>
+								<input type='radio' name='premium' />
+								<label className={styles.smaller_label} htmlFor='premium'>
+									Green Premium
+								</label>
+							</div>
+							<div className={styles.second_info_box}>
+								<p className={styles.bigger_label}>Ukształtowanie terenu:</p>
+								<input type='radio' name='flat' />
+								<label className={styles.smaller_label} htmlFor='mini'>
+									Płaski
+								</label>
+								<input type='radio' name='wrinkled' />
+								<label className={styles.smaller_label} htmlFor='standard'>
+									Pofałdowany
+								</label>
+								<input type='radio' name='steep' />
+								<label className={styles.smaller_label} htmlFor='premium'>
+									Stromy
+								</label>
+							</div>
+						</div>
+						<div className={styles.additional_info}>
+							<p>Dodatkowe informacje:</p>
+							<input type='text' />
+							<input type='radio' name='accept' />
+							<label className={styles.smaller_label} htmlFor='mini'>
+								Akceptuję politykę prywatności*
+							</label>
+						</div>
+						<button>Wyślij</button>
+					</form>
 				</div>
 			</Container>
 		</div>
